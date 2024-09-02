08ce018377a7078d7358d95a236568e837a721a7
Petr Friedrich <petr.friedrich2@gmail.com>
2024-09-02 14:42:42 +0200

    Editing pipeline

--- src/utils/calculate-age.ts
+++ src/utils/calculate-age.ts
@@ -4,6 +4,9 @@ function transformToValidISO8601(dateStr: string) {
   return dateStr.replace(/\s+([+-]\d{2}:\d{2})$/, "$1");
 }
 
+/**
+ * @param isoDateString ISO8601: "2015-04-02T11:39:43 -03:00". Including space between time and zone.
+ */
 export function calculateAge(isoDateString: string) {
   const birthDate = DateTime.fromISO(transformToValidISO8601(isoDateString));
 
